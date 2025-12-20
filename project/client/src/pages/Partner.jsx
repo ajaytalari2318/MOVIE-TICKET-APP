c945bcdcedd299ede2f0a8647a0cf71a11339233
ajaytalari2318 <ajaytalari2318@gmail.com>
2025-12-20 11:26:46 +0530

    push

--- project/client/src/pages/Partner.jsx
+++ project/client/src/pages/Partner.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Partner() {
-  return (
-    <div>Partner</div>
-  )
+    return (
+        <div>Partner</div>
+    )
 }
 
 export default Partner
---
+++ project/client/src/pages/PartnerProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PartnerProfile() {
+  return (
+    <div>PartnerProfile</div>
+  )
+}
+
+export default PartnerProfile
